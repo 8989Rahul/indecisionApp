37b917632c020b5f03c3d7f2350a78d7b219705c
8989Rahul <daghalerahul0@gmail.com>
2019-07-20 16:01:42 +0530

    importing header,options,option create optionSubmite method pushing some options passing props  to  header ,  options , option

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Options from "./components/Options";
+import Option from "./components/Option";
 
 export default class App extends Component {
   state = {
     title: "Indecision App",
-    subtitle: "put your life in the hand of a computer",
-    location: "hgjy",
+    subTitle: "put your life in the hand of a computer",
     options: [],
     visibility: false
   };
 
-  getLocation = location => (location ? location : "Undefine");
-
-  formSubmite = e => {
-    e.preventDefault();
-
-    let options = [];
-    let value = e.target.elements.option.value;
-    if (value) {
-      options.push(value);
-      e.target.elements.option.value = "";
+  optionSubmite = value => {
+    if (!value) {
+      return "Enter a valid value";
+    } else if (this.state.options.indexOf(value) > -1) {
+      return "This value already exist";
     }
 
-    this.setState({ options: [...this.state.options, ...options] }, () =>
-      console.log(this.state.options)
-    );
+    this.setState(prevState => {
+      return { options: prevState.options.concat(value) };
+    });
   };
 
   removeAll = () => this.setState({ options: [] });
@@ -38,6 +35,7 @@ export default class App extends Component {
   onVisibility = () => {
     let visibility = this.state.visibility;
     console.log(visibility);
+    console.log();
 
     this.setState({ visibility: !visibility });
     console.log(this.state.visibility);
@@ -53,30 +51,13 @@ export default class App extends Component {
         </button>
         {this.state.visibility && (
           <div>
-            <h1>{this.state.title}</h1>
-            <h2>{this.state.subtitle}</h2>
-            <p>Location : {this.getLocation(this.state.location)}</p>
-            <p>
-              {this.state.options.length === 0
-                ? "No Options"
-                : "Here are Your Options"}
-            </p>
-            <button onClick={this.removeAll}>Remove all</button>
-            <button
-              disabled={this.state.options.length === 0}
-              onClick={this.onRandom}
-            >
-              Random
-            </button>
-            <ol>
-              {this.state.options.map((option, i) => (
-                <li key={i}>{option}</li>
-              ))}
-            </ol>
-            <form onSubmit={this.formSubmite}>
-              <input type="text" name="option" />
-              <button>Add Option</button>
-            </form>
+            <Header title={this.state.title} subTitle={this.state.subTitle} />
+            <Options
+              options={this.state.options}
+              removeAll={this.removeAll}
+              onRandom={this.onRandom}
+            />
+            <Option optionSubmite={this.optionSubmite} />
           </div>
         )}
       </div>
